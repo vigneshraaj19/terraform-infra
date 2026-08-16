4bddf25340292b70cfbecfe5af41bb7f8e0d7e7f
Vignesh Raaj <vigneshraaj19@gmail.com>
2026-08-16 18:06:45 +0530

    Initail commit12

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-  //asfaf
 
   # Created by terraform-bootstrap/ — run that once first, then fill in
   # the bucket name below (Terraform backend blocks can't use variables).
